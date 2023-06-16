36581f7327b3c7cd562c0efb38e80d80143719da
Khaled Nehad <khalednehad@gmail.com>
2023-06-16 20:39:04 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>eCommerce App</header>
+      <main></main>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
