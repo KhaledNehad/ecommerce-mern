d043568437a97d9a6e6ea8e3b07e2d9b64e6ecd1
Khaled Nehad <khalednehad@gmail.com>
2023-06-16 21:15:04 +0200

    wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css';
+import { sampleProduct } from './data';
 
 function App() {
   return (
     <div>
       <header>eCommerce App</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProduct.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className='product-image'
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>footer</footer>
     </div>
   );
