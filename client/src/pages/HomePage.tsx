78841c088e58a4dd4f7ff89fcce9c492c6c82b08
Khaled Nehad <khalednehad@gmail.com>
2023-06-17 23:16:31 +0200

    add some components

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import { Col, Row } from 'react-bootstrap';
-import { sampleProduct } from '../data';
 import { Link } from 'react-router-dom';
 import { Product } from '../types/Product';
 import { useEffect, useReducer } from 'react';
@@ -8,6 +7,8 @@ import { getError } from '../utils';
 import { ApiError } from '../types/ApiError';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import ProductItem from '../components/ProductItem';
+import { Helmet } from 'react-helmet-async';
 
 type State = {
   products: Product[];
@@ -71,17 +72,12 @@ export default function HomePage() {
     <MessageBox variant='danger'>{error}</MessageBox>
   ) : (
     <Row>
+      <Helmet>
+        <title>eCommerce Homepage</title>
+      </Helmet>
       {products.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
-          <Link to={'/product/' + product.slug}>
-            <img
-              src={product.image}
-              alt={product.name}
-              className='product-image'
-            />
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </Link>
+          <ProductItem product={product} />
         </Col>
       ))}
     </Row>
--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,3 +1,12 @@
+import { Helmet } from 'react-helmet-async';
+
 export default function ProductPage() {
-  return <div>ProductPage</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Product page</title>
+      </Helmet>
+      <div>ProductPage</div>
+    </div>
+  );
 }
