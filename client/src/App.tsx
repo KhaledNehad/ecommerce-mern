c9aaeda2bcd5d3b88aa570ba18b5c0dbedae3369
Khaled Nehad <khalednehad@gmail.com>
2023-06-17 14:26:12 +0200

    add bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,44 @@
-import './App.css';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { sampleProduct } from './data';
 
 function App() {
   return (
-    <div>
-      <header>eCommerce App</header>
+    <div className='d-flex flex-column vh-100'>
+      <header>
+        <Navbar bg='dark' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>eCommerce MERN</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href='/cart' className='nav-link'>
+              Cart
+            </a>
+            <a href='/signin' className='nav-link'>
+              Sing In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProduct.map((product) => (
-            <li key={product.slug}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className='product-image'
-              />
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className='mt-3'>
+          <Row>
+            {sampleProduct.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className='product-image'
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>footer</footer>
+      <footer>
+        <div className='text-center'>footer</div>
+      </footer>
     </div>
   );
 }
