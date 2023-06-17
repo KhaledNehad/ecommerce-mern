3852f001a8acbbd3505c3557a49874b71760dd69
Khaled Nehad <khalednehad@gmail.com>
2023-06-17 21:51:02 +0200

    setup server, frontend fetching data

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+import { sampleProduct } from './data';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  })
+);
+
+app.get('/api/products', (req: Request, res: Response) => {
+  res.json(sampleProduct);
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
---
+++ client/src/components/MessageBox.tsx
@@ -0,0 +1,11 @@
+import { Alert } from 'react-bootstrap';
+
+export default function MessageBox({
+  variant = 'info',
+  children,
+}: {
+  variant?: string;
+  children: React.ReactNode;
+}) {
+  return <Alert variant={variant}>{children}</Alert>;
+}
