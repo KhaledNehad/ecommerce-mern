2100e985f914243969b775e62d0c56a29f5421fe
Khaled Nehad <khalednehad@gmail.com>
2023-06-19 13:09:34 +0200

    dark mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,14 +14,13 @@ import ProductPage from './pages/ProductPage.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StoreProvider } from './store.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} element={<HomePage />} />
       <Route path='product/:slug' element={<ProductPage />} />
-      {/* <Route path='dashboard' element={<Dashboard />} /> */}
-      {/* ... etc. */}
     </Route>
   )
 );
@@ -30,11 +29,13 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <StoreProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>
 );
